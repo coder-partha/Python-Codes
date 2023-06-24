2109c72534a179f4b7e9b33ebc3dadc3ef7b4f5a
Partha <137587210+coder-partha@users.noreply.github.com>
2023-06-24 08:20:47 +0530

    Create 3-15-escape-sequences.py

---
+++ 3-15-escape-sequences.py
@@ -0,0 +1,11 @@
+# Escape Sequences
+
+weather = "It\'s \"kind of\" sunny"
+print(weather)
+
+# Tab \t New line \n
+weather = "\t It\'s \"kind of\" sunny \n hope you have a good day!"
+
+print(weather)
+
+
